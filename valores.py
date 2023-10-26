85395c3aa771e48df9ea66e8a0ae9971e1ba04f4
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-26 05:50:00 -0600

    Practica mosquitto grafica

---
+++ valores.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([7, 100,6,250,8,400,9,600])
+
+#ypoints = np.array([6, 250])
+
+#zpoints = np.array([8, 400])
+
+#wpoints = np.array([9, 600])
+plt.plot(xpoints , marker = 'o')
+plt.show()
